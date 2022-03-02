f2cdc191827e82f18eae30c295ab94cb8888c3aa
Melvyn Koh <melvynkoh94@gmail.com>
2022-03-02 21:25:45 +0800

    Implement simple changes to test deployment effects

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Celebrate with us on 1st October 2022!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.swissotel.com/hotels/singapore-stamford/dining/skai/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Swissotel The Stamford
         </a>
       </header>
     </div>
