2501db09372f45f984fea01327a61f15b25abf24
Melvyn Koh <melvynkoh94@gmail.com>
2022-03-02 23:15:15 +0800

    Added some sample logos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import meowxuanlogo from './melvyn-isabelle-logo2.png';
+import swissotellogo from './swissotel.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={meowxuanlogo} alt="meowxuanlogo" />
         <p>
-          Celebrate with us on 1st October 2022!
+          Celebrate with us on 1st October 2022
         </p>
         <a
           className="App-link"
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Swissotel The Stamford
+          <img src={swissotellogo} alt="swissotel-logo" />
         </a>
       </header>
     </div>
